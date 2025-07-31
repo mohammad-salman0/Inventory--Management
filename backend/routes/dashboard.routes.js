912c50e25d88ca9ffc3e546855db22bf99335bb7
Abdul Wahid <89858008+AbdulWaihd@users.noreply.github.com>
2025-07-31 21:06:46 +0530

    Delete backend directory

--- backend/routes/dashboard.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboard.controller');
-
-
-router.get('/summary', dashboardController.getSalesSummary);
-router.get('/low-stock', dashboardController.getLowStockAlerts);
-
-module.exports = router;
